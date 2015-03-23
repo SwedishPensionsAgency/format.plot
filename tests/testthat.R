f4969df442aa40c5e0f0950f7696dd928fb8ca17
LCHansson <love.hansson@gmail.com>
2015-03-23 22:06:53 +0100

    Add testthat skeleton and mock test (to be completed when we have a working example)

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(format.plot)
+
+test_check("format.plot")
---
+++ tests/testthat/test-formatting.R
@@ -0,0 +1,9 @@
+library(format.plot)
+
+context("Plot output format")
+
+test_that("Plots generate properly to LaTeX", {
+  test_plotdata <- diamonds
+  # expect_equal_to_reference(format.tables(test_plotdata), "test_plotoutput.rds")
+  expect_equal(1, 1)
+})
